d79af9fbbf646526c192c7899566dac1071e8345
YiroYi <yiroyi@live.com>
2020-12-12 21:39:22 -0600

    Adding the data of the categories to the Categories screen

---
+++ data/dummy-data.js
@@ -0,0 +1,14 @@
+import Category from '../models/category';
+
+export const CATEGORIES = [
+  new Category('c1', 'Bug', '#f5428d'),
+  new Category('c2', 'Dark', '#f54242'),
+  new Category('c3', 'Dragon', '#f5a442'),
+  new Category('c4', 'Electric', '#f5d142'),
+  new Category('c5', 'Fighting', '#368dff'),
+  new Category('c6', 'Ice', '#41d95d'),
+  new Category('c7', 'Water', '#9eecff'),
+  new Category('c8', 'Fire', '#b9ffb0'),
+  new Category('c9', 'Steel', '#ffc7ff'),
+  new Category('c10', 'Rock', '#47fced')
+];
