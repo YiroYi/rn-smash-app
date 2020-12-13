47ecf99b2fbff6a5d1feac56511cbdcb94d546e9
YiroYi <yiroyi@live.com>
2020-12-12 22:20:15 -0600

    Adding functionality to render data in Pokemons

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, Platform } from 'react-native';
 import { CATEGORIES } from '../data/dummy-data';
+import Colors from '../constants/Colors';
 
 
 const CategoryMealScreen = props => {
@@ -25,6 +26,20 @@ const CategoryMealScreen = props => {
       />
     </View>
   );
+};
+
+CategoryMealScreen.navigationOptions = navigationData => {
+  const catId = navigationData.navigation.getParam('categoryId');
+
+  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
+  return {
+    headerTitle: selectedCategory.title,
+    headerStyle: {
+      backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+  };
 };
 
 const styles = StyleSheet.create({
