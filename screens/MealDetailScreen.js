af63732ff2c25f1bef60ee143d5acdbb575c0077
YiroYi <yiroyi@live.com>
2020-12-13 21:07:17 -0600

    Adding Start to meal detail

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button} from 'react-native';
 
+
 import { MEALS } from '../data/dummy-data';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+
+import HeaderButton from '../components/HeaderButton'
 
 const MealDetailScreen = props => {
   const mealId = props.navigation.getParam('mealId');
@@ -25,7 +29,20 @@ MealDetailScreen.navigationOptions = navigationData => {
   const mealId = navigationData.navigation.getParam('mealId');
   const selectedMeal = MEALS.find(meal => meal.id === mealId);
   return {
-    headerTitle: selectedMeal.title
+    headerTitle: selectedMeal.title,
+    headerRight: () => {
+      return (
+        <HeaderButtons HeaderButtonComponent={HeaderButton}>
+          <Item
+            title="Favorite"
+            iconName="ios-star"
+            onPress={() => {
+              console.log('Mark as favorite!');
+            }}
+          />
+        </HeaderButtons>
+      )
+    }
   };
 };
 
