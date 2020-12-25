74078fb8a2bda624dbd526da4dbc2963be2b60ef
YiroYi <yiroyi@live.com>
2020-12-25 02:44:58 -0600

    Adding filters to callback hook

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { View, Text, StyleSheet, Switch } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../components/HeaderButton';
@@ -19,10 +19,29 @@ const FilterSwitch = props => {
 
 
 const FiltersScreen = props => {
+  const { navigation } = props;
+
   const [isGlutenFree, setIsGlutenFree] = useState(false);
   const [isLactoseFree, setIsLactoseFree] = useState(false);
   const [isVegan, setIsVegan] = useState(false);
   const [isVegetarian, setIsVegetarian] = useState(false);
+
+  const saveFilters = useCallback(() => {
+    const appliedFilters = {
+      glutenFree: isGlutenFree,
+      lactoseFree: isLactoseFree,
+      vegan: isVegan,
+      vegetarian: isVegetarian
+    };
+
+    console.log(appliedFilters);
+  },[isGlutenFree, isLactoseFree, isVegan, isVegetarian]);
+
+  useEffect(() => {
+    navigation.setParams({save: saveFilters});
+  },[saveFilters]);
+
+
   return(
     <View style={styles.screen}>
       <Text style={styles.title}>Available Filters / Restrictions</Text>
@@ -45,11 +64,16 @@ const FiltersScreen = props => {
 FiltersScreen.navigationOptions = (navData) => {
   return {
     headerTitle: 'Your Favorites',
-    headerLeft: <HeaderButtons HeaderButtonComponent={HeaderButton}>
+    headerLeft: (<HeaderButtons HeaderButtonComponent={HeaderButton}>
                   <Item title="Menu" iconName='ios-menu' onPress={()=>{
                     navData.navigation.toggleDrawer();
                   }} />
-                </HeaderButtons>
+                </HeaderButtons>),
+    headerRight: (<HeaderButtons HeaderButtonComponent={HeaderButton}>
+                  <Item title="Save" iconName='ios-save' onPress={()=>{
+                    navData.navigation.getParam('save')();
+                  }} />
+                </HeaderButtons>)
   }
 }
 
