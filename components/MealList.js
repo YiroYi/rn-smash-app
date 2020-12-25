f7660a98f577c8d1e118cb117abb4e04d538aac3
YiroYi <yiroyi@live.com>
2020-12-25 00:25:28 -0600

    Populating favorite Item

--- components/MealList.js
+++ components/MealList.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import { View, FlatList, StyleSheet } from 'react-native';
 
+import MealItem from './MealItem';
 
 const MealList = props => {
+  const renderMealItem = itemData => {
+    return (
+      <MealItem
+        title={itemData.item.title}
+        image={itemData.item.imageUrl}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affordability={itemData.item.affordability}
+        onSelectMeal={() => {
+          props.navigation.navigate({
+              routeName: 'MealDetail',
+              params: {
+                mealId: itemData.item.id
+              }
+          });
+        }}
+      />
+    );
+  };
+
   return (
-    <View style={styles.screen}>
+    <View style={styles.list}>
       <FlatList
-        data={displayedMeals}
+        data={props.listData}
         renderItem={renderMealItem}
         style={{width: '100%'}}
       />
@@ -15,7 +36,12 @@ const MealList = props => {
 }
 
 const styles = StyleSheet.create({
-
+  list: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 15
+  }
 });
 
 export default MealList;
--- screens/FavoriteScreen.js
+++ screens/FavoriteScreen.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import MealList from '../components/MealList';
+
+import { MEALS } from '../data/dummy-data';
 
 const FavoriteScreen = props => {
+  const favMeals = MEALS.filter(meal => meal.id === 'm1' || meal.id === 'm2');
   return(
-    <View style={styles.screen}>
-      <Text>The Favorite Screen </Text>
-    </View>
+    <MealList listData={favMeals} navigation={props.navigation} />
   );
 };
 
@@ -13,12 +14,6 @@ FavoriteScreen.navigationOptions = {
   headerTitle: 'Your Favorites'
 };
 
-const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-});
+
 
 export default FavoriteScreen;
